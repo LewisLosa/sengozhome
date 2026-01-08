a36468fcad3f06b0d0d5491e59284a134fa8d27a
LewisLosa <lewislosa@gmail.com>
2026-01-08 18:36:10 +0300

    nix-conf moved to another repository.

--- nix-conf/vars.nix
+++
@@ -1,8 +0,0 @@
-{
-  fullName = "Eyüp Şengöz";
-  username = "eyups";
-  email = "lewislosa@gmail.com";
-  sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGDjtNxYKaRlCTu0MnDyq3AmRmSBGE10Lv+8RT4byE92 lewislosa@gmail.com";
-  timezone = "Europe/Istanbul";
-  keyMap = "trq";
-}
